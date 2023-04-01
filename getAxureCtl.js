2e9c840e0ffcd7e82eedffed23fe2816ce4632fe
blazex <674106780@qq.com>
2023-04-02 00:03:11 +0800

    update

--- getAxureCtl.js
+++ getAxureCtl.js
@@ -1,11 +1,12 @@
 function get_layout() {
-let html_head = `
+	let html_head = `
 <html>
 <head>
 <style>
 	* {
 		padding:0px;
 		margin:0px;
+		font-size:13px;
 	}
 	.ctldiv{
 		position: absolute;
@@ -19,98 +20,87 @@ let html_head = `
 </head>
 <body>
 `
-let html_footer =`
+	let html_footer = `
 </body>
 </html>
 `
+	function f_div_wrap(x, main) {
+		return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}"> ${main} </div> `
+	}
 
-	let  convert_dict = {
-		button: (x)=> {
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}"> `+
-			`<button id = '${x.name}' > ${x.innerText}</button> </div>`
-
-		},
-		primary_button: (x)=> {
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}" >` +
-			`<button id = '${x.name}'> ${x.innerText}</button> </div>`
-
-		},
-		radio_button: (x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}"> `+
-			`<div><input type="radio" id = "${x.name}" value="${x.innerText}" >${x.innerText} </div></div>`
-		},
-		checkbox: (x) => {
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}"> `+
-			`<div><input type="checkbox" id = "${x.name}" value="${x.innerText}" >${x.innerText}</div></div>`
-
-		},
-		list_box:(x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}">`+
-			`<select id = "${x.name}" size = "2"> ${x.options}</select></div>`
-		},
-		droplist:(x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}">`+
-			`<select id = "${x.name}" size = "1" > ${x.options}</select></div>`
-		},
-		text_area: (x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}">`+
-			`<textarea id = '${x.name}' > ${x.innerText}</textarea></div>`
-
-		},
-		text_field: (x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}"> `+
-			`<input type="text" id = "${x.name}" value="${x.value}" ></div>`
-		},
-		paragraph: (x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}">`+
-			`<p id = '${x.name}' > ${x.innerText}</p> </div>`
-		},
-		label:(x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}">`+
-			`<label id = '${x.name}' > ${x.innerText}</label></div>`
-		},
-		image:(x)=>{
-			return `<div class = "ctlDiv" style = "left:${x.left};top:${x.top};width:${x.width};height:${x.height}">`+
-			`<img id = '${x.name}' src = '${x.src}'> </div>`
-		}
+	let convert_dict = {
+		button: (x) => f_div_wrap(x, `<button id = '${x.name}' > ${x.innerText}</button>`),
+		primary_button: (x) => f_div_wrap(x, `<button id = '${x.name}'> ${x.innerText}</button>`),
+		radio_button: (x) => f_div_wrap(x, `<div><input type="radio" id = "${x.name}" value="${x.innerText}" >${x.innerText}</div>`),
+		checkbox: (x) => f_div_wrap(x, `<div><input type="checkbox" id = "${x.name}" value="${x.innerText}" >${x.innerText}</div>`),
+		list_box: (x) => f_div_wrap(x, `<select id = "${x.name}" size = "2"> ${x.options}</select>`),
+		droplist: (x) => f_div_wrap(x, `<select id = "${x.name}" size = "1" > ${x.options}</select>`),
+		text_area: (x) => f_div_wrap(x, `<textarea id = '${x.name}' > ${x.innerText}</textarea>`),
+		text_field: (x) => f_div_wrap(x, `<input type="text" id = "${x.name}" value="${x.value}" >`),
+		paragraph: (x) => f_div_wrap(x, `<p id = '${x.name}' > ${x.innerText}</p>`),
+		label: (x) => f_div_wrap(x, `<label id = '${x.name}' > ${x.innerText}</label>`),
+		image: (x) => f_div_wrap(x, `<img id = '${x.name}' src = '${x.src}'>`),
+		box_1: (x) => f_div_wrap(x, `<div id = '${x.name}' style="background-color:${x.background_color}">${x.innerText}</div>`),
+		box_2: (x) => f_div_wrap(x, `<div id = '${x.name}' style="background-color:${x.background_color}">${x.innerText}</div>`),
+		box_3: (x) => f_div_wrap(x, `<div id = '${x.name}' style="background-color:${x.background_color}">${x.innerText}</div>`)
 
 	}
 
 
 	let doc = document.querySelectorAll("[data-label]")
-	let arr = Array.from(doc,x => {
-			let computedStyle   =   window.getComputedStyle(x)
-			let out = {
-				id:x['id'],
-				name:x.getAttribute('data-label'),
-				class:x.getAttribute('class').split(' ')[1],
-				left: computedStyle.left,
-				top:computedStyle.top,
-				width:computedStyle.width,
-				height:computedStyle.height,
-				innerText:x.textContent.trim()
-			}
-			if (out.class ==='text_field'){
-				out['value'] = x.querySelector('input').value
-			}
-			if (out.class ==='list_box' |out.class ==='droplist' ){
-				out['options'] = x.querySelector('select').innerHTML.trim()
-			}
-			if (out.class ==='image'){
-				out['src'] = x.querySelector('img').src
-			}
-			return out 
-		
+	let arr = Array.from(doc, x => {
+		let computedStyle = window.getComputedStyle(x)
+		let out = {
+			id: x['id'],
+			name: x.getAttribute('data-label'),
+			class: x.getAttribute('class').split(' ')[1],
+			left: computedStyle.left,
+			top: computedStyle.top,
+			width: computedStyle.width,
+			height: computedStyle.height,
+			innerText: x.textContent.trim()
 		}
+		switch(out.class){
+			case 'text_field':
+				out['value'] = x.querySelector('input').value
+				break;
+			case 'list_box':
+			case 'droplist':
+				out['options'] = x.querySelector('select').innerHTML.trim()
+				break;
+			case 'image':
+				out['src'] = x.querySelector('img').src
+				break;
+			case 'box_1':
+			case 'box_2':
+			case 'box_3':
+				out['background_color'] = window.getComputedStyle(x.querySelector('div>:first-child')).backgroundColor
+				
+		}
+
+		
+		// if (out.class === 'text_field') {
+		// 	out['value'] = x.querySelector('input').value
+		// }
+		// if (out.class === 'list_box' | out.class === 'droplist') {
+		// 	out['options'] = x.querySelector('select').innerHTML.trim()
+		// }
+		// if (out.class === 'image') {
+		// 	out['src'] = x.querySelector('img').src
+		// }
+		return out
+
+	}
 	)
 	let properties = JSON.stringify(arr)
-	let html_main = arr.map( x => {
-		return Object.keys(convert_dict).includes(x.class)? convert_dict[x.class](x):null
+	let html_main = arr.map(x => {
+		return Object.keys(convert_dict).includes(x.class) ? convert_dict[x.class](x) : null
 	}).join("\n").trim()
 
 
 	return (html_head + html_main + html_footer).trim()
-} 
+}
 
 get_layout();
+
 
