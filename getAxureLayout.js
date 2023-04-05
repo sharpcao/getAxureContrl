9cc1fb5cd4fa0e636af89ddb8d476f94ff0b74ab
sharp <674106780@qq.com>
2023-04-05 21:23:07 +0800

    fix getLayout bug

--- getAxureLayout.js
+++ getAxureLayout.js
@@ -1,25 +1,25 @@
 let doc = document.querySelectorAll("[data-label]")
-let arr  = Array.from(doc, x=>{
-     let computedStyle = window.getComputedStyle(x)
-     return {
-            id: x['id'],
-            name: x.getAttribute('data-label'),
-            class: x.getAttribute('class').split(' ')[1],
-            left: parseInt(computedStyle.left),
-            top: parseInt(computedStyle.top),
-            width: parseInt(computedStyle.width),
-            height: parseInt(computedStyle.height),
-            innerText: x.textContent.trim(),
-            parent:"app"
-        }
+let arr = Array.from(doc, x => {
+    let computedStyle = window.getComputedStyle(x)
+    return {
+        id: x['id'],
+        name: x.getAttribute('data-label'),
+        class: x.getAttribute('class').split(' ')[1],
+        left: parseInt(computedStyle.left),
+        top: parseInt(computedStyle.top),
+        width: parseInt(computedStyle.width),
+        height: parseInt(computedStyle.height),
+        innerText: x.textContent.trim(),
+        parent: "app"
+    }
 
-}).sort((a,b)=>{ return parseInt(b.left) - parseInt(a.left) })
+}).sort((a, b) => { return parseInt(b.left) - parseInt(a.left) })
 
 
-for (let i = 0; i < arr.length-1; i++) {
-    for (let j = i+1; i < arr.length; j++) {
-        console.log(i,j)
-        if (isChild(arr[i],arr[j])){
+for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; i < arr.length; j++) {
+        console.log(i, j)
+        if (isChild(arr[i], arr[j])) {
             arr[i]['parent'] = arr[j]['name']
             break
         }
@@ -27,24 +27,27 @@ for (let i = 0; i < arr.length-1; i++) {
 }
 
 
-function getChildren(parentName=""){
-    return arr.filter(x=>(x.parent===parentName))
-}
-function getNode(node){
-    return arr.filter(x=>(x.name==node))[0]
-}
-
-function isChild(child,parent){
-    return (child.left > parent.left & child.width < parent.width & 
-        child.top > parent.top & child.height < parent.height) ? true:false
+function getChildren(parentName = "") {
+    return arr.filter(x => (x.parent === parentName)).sort((a, b) => {
+        return Math.min(a.left + a.top, b.left + b.top)
+    })
 }
 
-function getLayoutDiv(name){
+function getNode(node) {
+    return arr.filter(x => (x.name == node))[0]
+}
+
+function isChild(child, parent) {
+    return (child.left > parent.left & child.left+child.width < parent.left+parent.width &
+        child.top > parent.top & child.top +child.height < parent.top+parent.height) ? true : false
+}
+
+function getLayoutDiv(name) {
 
     let node = getNode(name)
     if (node.length === 0) return ""
 
-    let out_html = `<div id = "${node.name}">\n`
+    let out_html = `<div id = "${node.name}" class="box">\n`
 
     let children = getChildren(name)
 
@@ -54,10 +57,21 @@ function getLayoutDiv(name){
     out_html += `</div>\n`
     return out_html
 }
-function get_axure_layout()
-{
+function get_css_style(){
+
+   let main_css = (arr.map(x=>{
+        return `#${x.name} {
+            width:${x.width}px;
+            height:${x.height}px;
+            }`
+   }).reverse()).join('\n')
+   return `<style> \n*{padding:0px;margin:0px} .box{padding:3px; 
+   border:1px solid red; box-sizing:border-box;} \n` + main_css + `\n</style>`
+}
+
+function get_axure_layout() {
     let node = getChildren("app")[0]
-    return getLayoutDiv(node.name)
+    return {div:getLayoutDiv(node.name), css:get_css_style()}
 }
 
 get_axure_layout()
